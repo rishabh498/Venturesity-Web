a57ec1c880c22b44f191b6c648b9660147e018c2
rishabh498 <rishabhyadav68@gmail.com>
2019-08-17 23:21:58 +0530

    Fixed Navbar

--- js/initialAnimation.js
+++ js/initialAnimation.js
@@ -2,4 +2,8 @@ var x = document.getElementById("backdrop");
 setTimeout(function()
 { 
     $("#backdrop").fadeTo("slow", 0);
- }, 5000);
+ }, 4000);
+setTimeout(function()
+{ 
+    x.remove();
+ }, 5500);
